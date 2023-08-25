fccd8f0a545bf1972396336a579404cfddf38b38
Ascalon <araujogleybson44@gmail.com>
2023-08-25 03:30:14 -0300

    projeto concluido!

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -52,7 +52,7 @@ while (exibirMenu)
             break;
     }
 
-    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.WriteLine("Pressione uma tecla para voltar ao menu");
     Console.ReadLine();
 }
 
